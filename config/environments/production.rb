7820b72700834ee459821f67dee73ffdb52f9c28
Peter Marcano <pbmarcano91@gmail.com>
2021-06-01 11:03:30 -0400

    Update name from Ephemeral Tweets to Tweet Sweeper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ephemeral_tweets_production"
+  # config.active_job.queue_name_prefix = "tweet_sweeper_production"
 
   config.action_mailer.perform_caching = false
 
