3719e0301c52cf6f2994c533ea2943ed0ccb49cd
Peter Marcano <pbmarcano91@gmail.com>
2021-08-04 10:01:47 -0400

    Job to delete tweets

---
+++ app/jobs/delete_tweet_job.rb
@@ -0,0 +1,30 @@
+class DeleteTweetJob < ApplicationJob
+  queue_as :default
+
+  def perform(tweet)
+    @tweet = tweet
+
+    begin
+      delete_from_twitter
+      tweet.destroy
+    rescue StandardError => e
+      puts e.inspect
+      puts "Error deleting #{tweet.id}; exiting"
+      exit
+    else
+      puts "Deleted #{tweet.id}"
+    end
+  end
+
+  private
+
+  attr_reader :tweet
+
+  def delete_from_twitter
+    client.destroy_status(tweet.tweet_id)
+  end
+
+  def client
+    @client ||= tweet.user.twitter
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_183840) do
+ActiveRecord::Schema.define(version: 2021_08_02_192927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.bigint "tweet_id", null: false
+    t.datetime "published_at", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_tweets_on_tweet_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_04_30_183840) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tweets", "users"
 end
