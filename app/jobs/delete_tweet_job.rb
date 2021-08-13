ff452712cecf7f5c73475da0a6ac83d45392bf33
Peter Marcano <pbmarcano91@gmail.com>
2021-08-13 15:36:55 -0400

    Clean up delete job

--- app/jobs/delete_tweet_job.rb
+++ app/jobs/delete_tweet_job.rb
@@ -2,19 +2,10 @@ class DeleteTweetJob < ApplicationJob
   queue_as :default
 
   def perform(tweet)
-    puts "DeleteTweetJob for #{tweet.tweet_id}"
     @tweet = tweet
 
-    # begin
     delete_from_twitter
     @tweet.destroy
-    # rescue StandardError => e
-    #   puts e.inspect
-    #   puts "Error deleting #{tweet.id}; exiting"
-    #   exit
-    # else
-    #   puts "Deleted #{tweet.id}"
-    # end
   end
 
   private
@@ -22,8 +13,6 @@ class DeleteTweetJob < ApplicationJob
   def delete_from_twitter
     client.destroy_status(@tweet.tweet_id)
   rescue Twitter::Error::NotFound
-    # if can't be found on twitter, delete locally
-    # @tweet.destroy
     return nil
   end
 
