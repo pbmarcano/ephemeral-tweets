96137b57f3a442d6e2294422cbc6e6d48f8a6eb4
Peter Marcano <pbmarcano91@gmail.com>
2021-09-03 19:52:05 -0400

    reorganize app javascript thing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// rails
 import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 ActiveStorage.start()
 
+// hotwire
+import "@hotwired/turbo-rails"
 import "controllers"
+
+// tailwind
 import "stylesheets/application.scss"
