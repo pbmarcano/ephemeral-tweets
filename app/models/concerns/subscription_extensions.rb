dca387cb94e902ec9a49e7d81042d6cc14c92b86
Peter Marcano <pbmarcano91@gmail.com>
2021-09-14 17:31:56 -0400

    Refactor subscription extension

--- app/models/concerns/subscription_extensions.rb
+++ app/models/concerns/subscription_extensions.rb
@@ -5,13 +5,9 @@ module SubscriptionExtensions
     after_update :disable_sweeping
   end
 
-  def disable_sweeping
-    disable_sweeping unless self.active?
-  end
-
   private
 
   def disable_sweeping
-    self.customer.owner.setting.disable_sweeping
+    self.customer.owner.setting.disable_sweeping unless self.active?
   end
 end
