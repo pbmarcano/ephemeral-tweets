095846452863a831cae01673f2c4f1849915cfbc
Peter Marcano <pbmarcano91@gmail.com>
2021-09-15 23:13:10 -0400

    TurboStream in tweets upon account creation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   scope :oldest_first, -> { order(published_at: :asc) }
+
+  after_create_commit do
+    broadcast_prepend_later_to user, :tweets, target: "tweets"
+  end
 end
