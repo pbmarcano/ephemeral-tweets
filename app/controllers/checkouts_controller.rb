fce9a564814667de4b73b07cf392ed2a460dbf0f
Peter Marcano <pbmarcano91@gmail.com>
2021-09-20 13:49:08 -0400

    Add payment product to production

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,7 +4,7 @@ class CheckoutsController < ApplicationController
   def show
     @checkout_session = current_user.payment_processor.checkout(
       mode: "subscription",
-      line_items: "price_1JZKWWGRMpXpIAvQkbbtgQnE",
+      line_items: Rails.application.credentials.dig(:stripe, :standard_product)
       success_url: dashboard_url,
       cancel_url: dashboard_url
     )
