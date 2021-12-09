65a560603aeb304e510b375d2b8daded071eb5bd
Peter Marcano <pbmarcano91@gmail.com>
2021-12-09 11:02:26 -0600

    remove current_user reference.
    - add caching to avoid n+1 queries per view

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,10 @@ class Tweet < ApplicationRecord
   belongs_to :user
   scope :oldest_first, -> { order(published_at: :asc) }
 
+  delegate :name, to: :user
+  delegate :profile_image, to: :user
+  delegate :username, to: :user
+
   after_create_commit do
     broadcast_prepend_later_to user, :tweets, target: "tweets"
   end
