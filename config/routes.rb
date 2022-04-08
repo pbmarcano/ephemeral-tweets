5e0eefe89bc9b8299b84ac6e686d0350b55469d8
Peter Marcano <pbmarcano91@gmail.com>
2022-04-08 12:12:01 -0600

    Slim down weekly stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
   namespace :admin do
     mount Blazer::Engine, at: "/blazer"
     mount Sidekiq::Web, at: '/sidekiq'
