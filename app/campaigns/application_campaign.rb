f8d9cab15bd6a5be49c0b25f1a9fb9e58fb764a6
Peter Marcano <pbmarcano91@gmail.com>
2022-05-13 21:14:27 -0700

    Don't email anyone without an email address

--- app/campaigns/application_campaign.rb
+++ app/campaigns/application_campaign.rb
@@ -1,3 +1,5 @@
 class ApplicationCampaign < Heya::Campaigns::Base
-  default from: ActionMailer::Base.email_address_with_name("peter@tweetsweeper.app", "Pete from Tweet Sweeper")
+  segment { |u| u.email.present? }
+
+  default from: ActionMailer::Base.email_address_with_name("peter@tweetsweeper.app", "Pete via Tweet Sweeper")
 end
