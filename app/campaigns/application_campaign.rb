167a19f7a2a686148338a458eb377711dc55be6a
Peter Marcano <pbmarcano91@gmail.com>
2022-05-15 17:21:19 -0700

    don't make error method private

--- app/campaigns/application_campaign.rb
+++ app/campaigns/application_campaign.rb
@@ -1,12 +1,10 @@
 class ApplicationCampaign < Heya::Campaigns::Base
-  rescue_from Postmark::InactiveRecipientError, with: :log_error
+  rescue_from Postmark::InactiveRecipientError, with: :log_bad_email_error
 
   segment { |u| u.email.present? }
   default from: ActionMailer::Base.email_address_with_name("peter@tweetsweeper.app", "Pete via Tweet Sweeper"), bcc: "peter@tweetsweeper.app"
 
-  private
-
-  def log_error(error)
+  def log_bad_email_error(error)
     ErrorMailer.with(error: error).bad_user_email.deliver_later
     Rails.logger.error("ActionMailer error: #{error}")
   end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  rescue_from Postmark::InactiveRecipientError, with: :log_error
+  rescue_from Postmark::InactiveRecipientError, with: :log_bad_email_error
 
-  default from: ActionMailer::Base.email_address_with_name("peter@tweetsweeper.app", "Pete from Tweet Sweeper")
+  default from: ActionMailer::Base.email_address_with_name("peter@tweetsweeper.app", "Pete via Tweet Sweeper")
   layout 'mailer'
 
-  private
-
-  def log_error(error)
+  def log_bad_email_error(error)
     ErrorMailer.with(error: error).bad_user_email.deliver_later
     Rails.logger.error("ActionMailer error: #{error}")
   end
