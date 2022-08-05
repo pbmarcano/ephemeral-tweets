8aa7694829cd765e06590c482dd71cc115ee8716
Peter Marcano <pbmarcano91@gmail.com>
2022-08-05 14:35:33 -0400

    only show sweeping users in social proof

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,12 @@ class HomesController < ApplicationController
   layout "marketing"
 
   def show
-    @social_proof_users = User.last(5).reverse
+    @social_proof_users = sweeping_users
+  end
+
+  private
+
+  def sweeping_users
+    User.enabled_sweeping.last(5).reverse
   end
 end
