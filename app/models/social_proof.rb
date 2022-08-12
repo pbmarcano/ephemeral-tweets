1e793fd4b0d4756f80eefd3df14ac307d5435619
Peter Marcano <pbmarcano91@gmail.com>
2022-08-12 17:13:58 -0400

    Give cache a name so it works

--- app/models/social_proof.rb
+++ app/models/social_proof.rb
@@ -1,6 +1,6 @@
 class SocialProof
   def users
-    ids = Rails.cache.fetch("", expires_in: 12.hours) do 
+    ids = Rails.cache.fetch("social_proof_users", expires_in: 12.hours) do 
       User.enabled_sweeping.last(5).reverse.pluck(:id)
     end
 
