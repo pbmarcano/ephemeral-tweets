b6e46ab4d2a8a752cf01816a7c6aee062f47600d
Peter Marcano <pbmarcano91@gmail.com>
2022-09-27 14:58:06 -0400

    Upcoming mailer shows which tweets will be deleted

--- app/mailers/upcoming_mailer.rb
+++ app/mailers/upcoming_mailer.rb
@@ -7,11 +7,15 @@ class UpcomingMailer < ApplicationMailer
   #
   def notify(user)
     @user = user
+    @grouped_tweets = @user.upcoming_tweets_this_week.group_by_deletion_date
     
     tweet_count = @user.upcoming_tweets_this_week.count
 
     if tweet_count > 0
-      mail to: @user.email, subject: default_i18n_subject(count: tweet_count)
+      mail to: @user.email, 
+        subject: default_i18n_subject(
+          number_of_tweets: "#{tweet_count} #{"tweet".pluralize(tweet_count)}"
+        )
     end
   end
 end
